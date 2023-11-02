72adec57338d7b1adb20038b0666dae92cfcc57d
pooja-samrit <149418614+pooja-samrit@users.noreply.github.com>
2023-11-02 21:02:12 +0530

    Add files via upload

---
+++ ArithmethicOperation.java
@@ -0,0 +1,21 @@
+package accessspec1;
+
+public class ArithmethicOperation 
+{
+	private	void add() 
+	{
+		System.out.println("1");
+	}
+	public void sub()
+	{
+		System.out.println("2");
+	}
+	protected void mult()
+	{
+		System.out.println("3");
+	}
+    void div()
+   {
+    	System.out.println("4");
+   }
+}
